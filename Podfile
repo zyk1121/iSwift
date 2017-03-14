3b88a64831ba8e2d1ff99d2adb6420c34cfe8881
张元科 <zhangyuanke@leador.com.cn>
2017-03-14 22:39:56 +0800

    init project

---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+use_frameworks!
+
+platform :ios, "8.0"
+
+xcodeproj 'iStudySwift/iStudySwift.xcodeproj'
+
+target "iStudySwift" do
+  pod 'SnapKit', '~> 0.19.1'
+end
---
+++ iStudySwift/iStudySwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iStudySwift
+//
+//  Created by zhangyuanke on 17/3/14.
+//  Copyright © 2017年 zhangyuanke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
