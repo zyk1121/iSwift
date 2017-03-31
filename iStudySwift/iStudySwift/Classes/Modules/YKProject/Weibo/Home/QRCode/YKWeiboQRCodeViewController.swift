347c7d8c432e89a2df2e84f9017481954322ed8b
张元科 <zhangyuanke@leador.com.cn>
2017-03-31 22:48:44 +0800

    生产二维码ok

--- iStudySwift/iStudySwift/Classes/Modules/YKProject/Weibo/Home/QRCode/YKWeiboQRCodeViewController.swift
+++ iStudySwift/iStudySwift/Classes/Modules/YKProject/Weibo/Home/QRCode/YKWeiboQRCodeViewController.swift
@@ -14,7 +14,7 @@ import AVFoundation
 // 注意，页面Portal跳转不成功，请在YKPortal方法中注册类的初始化方法
 let kYKQRCodeURLString = "yk://istudydemo/weibo/home/qrcode"
 
-class YKQRCodeViewController: UIViewController,UITabBarDelegate,AVCaptureMetadataOutputObjectsDelegate {
+class YKQRCodeViewController: UIViewController,UITabBarDelegate {
     
     // MARK: - Portal 相关
     static func portalLoad()
@@ -287,10 +287,71 @@ class YKQRCodeViewController: UIViewController,UITabBarDelegate,AVCaptureMetadat
         print("相册")
     }
     
+    /**
+     扫描二维码
+     */
+//    private func startScan(){
+//        
+//        // 1.判断是否能够将输入添加到会话中
+//        if !session.canAddInput(deviceInput)
+//        {
+//            return
+//        }
+//        // 2.判断是否能够将输出添加到会话中
+//        if !session.canAddOutput(output)
+//        {
+//            return
+//        }
+//        // 3.将输入和输出都添加到会话中
+//        session.addInput(deviceInput)
+//        print(output.availableMetadataObjectTypes)
+//        session.addOutput(output)
+//        print(output.availableMetadataObjectTypes)
+//        
+//        // 4.设置输出能够解析的数据类型
+//        // 注意: 设置能够解析的数据类型, 一定要在输出对象添加到会员之后设置, 否则会报错
+//        output.metadataObjectTypes =  output.availableMetadataObjectTypes
+//        print(output.availableMetadataObjectTypes)
+//        // 5.设置输出对象的代理, 只要解析成功就会通知代理
+//        output.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+//        // 如果想实现只扫描一张图片, 那么系统自带的二维码扫描是不支持的
+//        // 只能设置让二维码只有出现在某一块区域才去扫描
+//        //        output.rectOfInterest = CGRectMake(0.0, 0.0, 1, 1)
+//        
+//        // 添加预览图层
+//        view.layer.insertSublayer(previewLayer, atIndex: 0)
+//        
+//        // 添加绘制图层到预览图层上
+//        previewLayer.addSublayer(drawLayer)
+//        
+//        // 6.告诉session开始扫描
+//        session.startRunning()
+//    }
+//    
+
+    
     
     // 
     var session : AVCaptureSession?
-    var layer: AVCaptureVideoPreviewLayer?;
+    var previewLayer: AVCaptureVideoPreviewLayer?;
+    
+//    // 拿到输入设备
+//    private lazy var deviceInput: AVCaptureDeviceInput? = {
+//        // 获取摄像头
+//        let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+//        do{
+//            // 创建输入对象
+//            let input = try AVCaptureDeviceInput(device: device)
+//            return input
+//        }catch
+//        {
+//            print(error)
+//            return nil
+//        }
+//    }()
+    
+    // 拿到输出对象
+    private lazy var output: AVCaptureMetadataOutput = AVCaptureMetadataOutput()
     
     func scanQRcode()
     {
@@ -318,13 +379,37 @@ class YKQRCodeViewController: UIViewController,UITabBarDelegate,AVCaptureMetadat
                 output.metadataObjectTypes = [AVMetadataObjectTypeQRCode];
             }
             
+            // 如果想实现只扫描一张图片, 那么系统自带的二维码扫描是不支持的
+            // 只能设置让二维码只有出现在某一块区域才去扫描
+            // 右上角有（0 0 ）
+            let qrRect : CGRect = qrCodeView.frame;
+            let mainRect : CGRect = UIScreen.main.bounds
+            /*
+             let x:CGFloat = CGFloat(mainRect.width - qrRect.width - qrRect.origin.x) / CGFloat(mainRect.width)
+             let y:CGFloat = CGFloat(qrRect.origin.y) / CGFloat(mainRect.height)
+             let w:CGFloat = CGFloat(qrRect.width) / CGFloat(mainRect.width)
+             let h:CGFloat = CGFloat(qrRect.height) / CGFloat(mainRect.height)
+ 
+            let x:CGFloat = (qrRect.origin.x) / mainRect.width
+            let y:CGFloat = (qrRect.origin.y + 64) / mainRect.height
+            let w:CGFloat = qrRect.width / mainRect.width
+            let h:CGFloat = qrRect.height / mainRect.height
+             output.rectOfInterest = CGRect(x: x, y: y, width: w, height: h)
+          */
             ////添加图层
-            self.layer = AVCaptureVideoPreviewLayer(session:self.session!);
-            self.layer?.frame = self.view.frame;
-            self.view.layer.insertSublayer(self.layer!, at: 0);
+            self.previewLayer = AVCaptureVideoPreviewLayer(session:self.session!);
+            self.previewLayer?.frame = self.view.frame;
+            self.view.layer.insertSublayer(self.previewLayer!, at: 0);
+            
+            // 添加绘制图层到预览图层上
+            self.previewLayer?.addSublayer(drawLayer)
             
             //// 开始扫描
             self.session?.startRunning();
+            
+            // startRunning之后
+            output.rectOfInterest  =  (self.previewLayer?.metadataOutputRectOfInterest(for: qrRect))!
+            
         }
         catch let error as NSError
         {
@@ -332,43 +417,138 @@ class YKQRCodeViewController: UIViewController,UITabBarDelegate,AVCaptureMetadat
         }
     }
     
+//    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+//        let stringValue:String?;
+//        
+//        if (metadataObjects.count > 0)
+//        {
+//            let metadataObject = metadataObjects[0] as! AVMetadataMachineReadableCodeObject;
+//            
+//            ////
+//            stringValue = metadataObject.stringValue;
+//            if (nil != stringValue) ////这里捕捉到二维码
+//            {
+//                self.session!.stopRunning();
+//                //// 移除图层
+//                self.layer!.removeFromSuperlayer();
+//                
+//                ////启动拨号～～
+//                print("\(stringValue)")
+//                goBack()
+////                UIApplication.shared.openURL(NSURL(string: "tel://\(stringValue)")! as URL);
+//                
+//                
+//                //                let alertView = UIAlertController(title: "二维码", message: stringValue, preferredStyle: .Alert);
+//                //                let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil);
+//                //                alertView.addAction(action);
+//                
+//                ////显示提示框
+//                //                self.presentViewController(alertView, animated: true, completion:nil);
+//            }
+//            else
+//            {
+//                let alertView = UIAlertController(title: "二维码", message: "没有扫描到二维码", preferredStyle: .alert);
+//                let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil);
+//                alertView.addAction(action);
+//                
+//                ////显示提示框
+//                self.present(alertView, animated: true, completion:nil);
+//            }
+//        }
+//    }
+    
+    // 创建用于绘制边线的图层
+    lazy var drawLayer: CALayer = {
+        let layer = CALayer()
+        layer.frame = UIScreen.main.bounds
+        return layer
+    }()
+}
+extension YKQRCodeViewController: AVCaptureMetadataOutputObjectsDelegate
+{
+    // 只要解析到数据就会调用
     func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
-        let stringValue:String?;
+        // 0.清空图层
+        clearConers()
         
-        if (metadataObjects.count > 0)
+        // 1.获取扫描到的数据
+        // 注意: 要使用stringValue
+//        resultLabel.text = metadataObjects.last?.stringValue
+//        resultLabel.sizeToFit()
+        
+        // 2.获取扫描到的二维码的位置
+        // 2.1转换坐标
+        for object in metadataObjects
         {
-            let metadataObject = metadataObjects[0] as! AVMetadataMachineReadableCodeObject;
-            
-            ////
-            stringValue = metadataObject.stringValue;
-            if (nil != stringValue) ////这里捕捉到二维码
+            // 2.1.1判断当前获取到的数据, 是否是机器可识别的类型
+            if object is AVMetadataMachineReadableCodeObject
             {
-                self.session!.stopRunning();
-                //// 移除图层
-                self.layer!.removeFromSuperlayer();
-                
-                ////启动拨号～～
-                print("\(stringValue)")
-                goBack()
-//                UIApplication.shared.openURL(NSURL(string: "tel://\(stringValue)")! as URL);
-                
-                
-                //                let alertView = UIAlertController(title: "二维码", message: stringValue, preferredStyle: .Alert);
-                //                let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil);
-                //                alertView.addAction(action);
-                
-                ////显示提示框
-                //                self.presentViewController(alertView, animated: true, completion:nil);
-            }
-            else
-            {
-                let alertView = UIAlertController(title: "二维码", message: "没有扫描到二维码", preferredStyle: .alert);
-                let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil);
-                alertView.addAction(action);
-                
-                ////显示提示框
-                self.present(alertView, animated: true, completion:nil);
+                // 2.1.2将坐标转换界面可识别的坐标
+                let codeObject = previewLayer?.transformedMetadataObject(for: object as! AVMetadataObject) as! AVMetadataMachineReadableCodeObject
+                // 2.1.3绘制图形
+                drawCorners(codeObject: codeObject)
             }
         }
     }
+    
+    /**
+     绘制图形
+     
+     :param: codeObject 保存了坐标的对象
+     */
+    private func drawCorners(codeObject: AVMetadataMachineReadableCodeObject)
+    {
+        if codeObject.corners.isEmpty
+        {
+            return
+        }
+        
+        // 1.创建一个图层
+        let layer = CAShapeLayer()
+        layer.lineWidth = 4
+        layer.strokeColor = UIColor.red.cgColor
+        layer.fillColor = UIColor.clear.cgColor
+        
+        // 2.创建路径
+        let path = UIBezierPath()
+        var point = CGPoint(x: 0, y: 0)
+        var index: Int = 0
+        // 2.1移动到第一个点
+        // 从corners数组中取出第0个元素, 将这个字典中的x/y赋值给point
+        point = CGPoint(dictionaryRepresentation: (codeObject.corners[index] as! CFDictionary))!
+        path.move(to: point)
+        index += 1
+        
+        // 2.2移动到其它的点
+        while index < codeObject.corners.count
+        {
+            point = CGPoint(dictionaryRepresentation: (codeObject.corners[index] as! CFDictionary))!
+            index += 1
+            path.addLine(to: point)
+        }
+        // 2.3关闭路径
+        path.close()
+        
+        // 2.4绘制路径
+        layer.path = path.cgPath
+        
+        // 3.将绘制好的图层添加到drawLayer上
+        drawLayer.addSublayer(layer)
+    }
+    /**
+     清空边线
+     */
+    private func clearConers(){
+        // 1.判断drawLayer上是否有其它图层
+        if drawLayer.sublayers == nil || drawLayer.sublayers?.count == 0{
+            return
+        }
+        
+        // 2.移除所有子图层
+        for subLayer in drawLayer.sublayers!
+        {
+            subLayer.removeFromSuperlayer()
+        }
+    }
 }
+
